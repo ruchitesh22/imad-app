386b27f3665357a0db8297149b0eed254d4ba8f6
ruchitesh22 <32912048+ruchitesh22@users.noreply.github.com>
2018-04-23 13:45:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+app,get('/the-first-file',function(req,res){
+    res.send('This is the first file !!');
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
