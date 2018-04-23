00e010d1fc11e148b57e97d68e76149ba30b3f61
ruchitesh22 <32912048+ruchitesh22@users.noreply.github.com>
2018-04-24 03:27:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var btn=document.getElementById('bt');
 btn.onclick=function(){
 var reqobj= new new XMLHttpRequest();
 reqobj.onreadystatechange = function() {
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+    //if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
        // Typical action to be performed when the document is ready:
        console.log(reqobj.responseText.toString());
        document.getElementById('c').innerHTML = reqobj.responseText.toString();
