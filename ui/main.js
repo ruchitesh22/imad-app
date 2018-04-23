25d67b827400b929dd7bd5368cf3221aa1954d7c
ruchitesh22 <32912048+ruchitesh22@users.noreply.github.com>
2018-04-24 03:16:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ var reqobj= new new XMLHttpRequest();
 reqobj.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       document.getElementById("c").innerHTML = reqobj.responseText;
+       document.getElementById("c").innerHTML = reqobj.responseText.toString();
     }
 };
 var btn=document.getElementById('bt');
 btn.onclick=function(){
 reqobj.open("GET", "http://ruchitesh22.imad.hasura-app.io/ruchitesh", true);
-reqobj.send();
+reqobj.send(null);
 };
