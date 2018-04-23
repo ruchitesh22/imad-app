52079c2ad72cec145b69b6e7af764d87a9d4b031
ruchitesh22 <32912048+ruchitesh22@users.noreply.github.com>
2018-04-23 22:54:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var data=document.getElementById('special');
 data.innerHTML='My New Data';
+var img=document.getElementById('myimg');
+img.onClick=function(){
+    img.style.marginLeft='100px';
+};
